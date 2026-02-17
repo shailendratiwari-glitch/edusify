e2a675f6832979fce9e93919d58a6d530a75cb8a
shailendratiwari-glitch <shailendratiwartiwari@gmail.com>
2026-02-17 11:48:55 +0530

    Fixing file casing for Vercel

--- src/Components/Program/Program.jsx
+++ src/Components/Program/Program.jsx
@@ -1,40 +1,40 @@
 import React from 'react'
 import './Program.css'
-import  Program1 from'../../assets/Program-1.png'
-import  Program2 from'../../assets/Program-2.png'
-import  Program3 from'../../assets/Program-3.png'
-import  Programicon1 from'../../assets/Program-icon-1.png'
-import  Programicon2 from'../../assets/Program-icon-2.png'
-import  Programicon3 from'../../assets/Program-icon-3.png'
+
+// IMPORTANT: Ensure these filenames match the EXACT casing in your folder
+import programImg from '../../assets/Program-1.png'
+import Program2 from '../../assets/Program-2.png'
+import Program3 from '../../assets/Program-3.png'
+import Programicon1 from '../../assets/program-icon-1.png'
+import Programicon2 from '../../assets/program-icon-2.png'
+import Programicon3 from '../../assets/program-icon-3.png'
 
 const Program = () => {
   return (
     <div className='programs' id='programs'>
-        <div className='program'>
-<img src={Program1}/>
-<div className="caption">
-    <img src= {Programicon1}/>
-    <p> Graduation Degree</p>
-</div>
+      <div className='program'>
+        <img src={programImg} alt="Graduation" />
+        <div className="caption">
+          <img src={Programicon1} alt="icon" />
+          <p> Graduation Degree</p>
         </div>
+      </div>
 
-        <div className='program'>
-<img src={Program2}/>
-<div className="caption">
-    <img src= {Programicon2}/>
-    <p> Master Degree</p>
-</div>
+      <div className='program'>
+        <img src={Program2} alt="Masters" />
+        <div className="caption">
+          <img src={Programicon2} alt="icon" />
+          <p> Master Degree</p>
         </div>
+      </div>
 
-
-        <div className='program'>
-<img src={Program3}/>
-<div className="caption">
-    <img src= {Programicon3}/>
-    <p> Post Graduation </p>
-</div>
+      <div className='program'>
+        <img src={Program3} alt="Post Grad" />
+        <div className="caption">
+          <img src={Programicon3} alt="icon" />
+          <p> Post Graduation </p>
         </div>
-      
+      </div>
     </div>
   )
 }
